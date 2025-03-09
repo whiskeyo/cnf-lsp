1e5d072ad537271a0db15c26b8d8e69beb8d5efa
Tomasz Woszczynski <duchowe50k@gmail.com>
2025-03-10 00:18:43 +0100

    Add lots of TODO completion items and example cnf file with all directives

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# Wireshark Conformance Language Server
+# Wireshark Asn2wrs Conformance Language Server
 
-tbd
+[whiskeyo](https://github.com/whiskeyo/)'s Wireshark Asn2Wrs Conformance Language Server is developed to make working with `*.cnf` files way easier. The language itself contains lots of directives and provides:
+
+- intuitive syntax highlighting based on Text Mate grammar,
+- documented directives based on official [Wireshark Developer's Guide](https://www.wireshark.org/docs/wsdg_html_chunked/index.html),
+- snippets to speed up coding.
+
+## Development specific to VS Code
+
+Run this command from the root of the repository:
+
+```bash
+npm install
+code .
+```
+
+After VS Code is started, press the <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>B</kbd> to build the extension. Then switch to _Run and Debug_ view and press _Launch Client_, or simply press <kbd>F5</kbd> - it will open the _[Extension Development Host]_ VS Code window. Now, editing any `*.cnf` file inside that window will be affected by any changes that are made to the extension. Edits made in the `server` directory will be rebuilt automatically, but to see their effect, restart the _[Extension Development Host]_ window, either by pressing the Refresh key (🔃) or by the keyboard shortcut <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>F5</kbd>.
+
+## Implementation is still missing these things
+
+- full syntax highlighting,
+- good LSP,
+- and way more, since it's the starting project.
--- server/src/handlers/completion.ts
+++ server/src/handlers/completion.ts
@@ -13,9 +13,9 @@ import { TextDocument } from "vscode-languageserver-textdocument";
 
 import log from "../utils/log";
 
-function createImportIncludeCompletion(label: string): CompletionItem {
+function createImportCompletion(label: string): CompletionItem {
   return {
-    label,
+    label: label,
     insertText: `${label} \${1:filepath}`,
     insertTextFormat: InsertTextFormat.Snippet,
     kind: CompletionItemKind.Method,
@@ -26,11 +26,78 @@ function createImportIncludeCompletion(label: string): CompletionItem {
   };
 }
 
-const completions = [
-  createImportIncludeCompletion("#.IMPORT"),
-  createImportIncludeCompletion("#.INCLUDE"),
-] as CompletionItem[];
+function createRenameCompletion(label: string, typename: string): CompletionItem {
+  return {
+    label: label,
+    insertText: label,
+    insertTextFormat: InsertTextFormat.Snippet,
+    kind: CompletionItemKind.Method,
+    detail: `Start block of renames for ${typename}.`,
+    documentation: `This directive in the Asn2wrs conformation file starts a block in which ${typename} can be renamed.`,
+  };
+}
 
+function createTodoCompletionItem(label: string): CompletionItem {
+  return {
+    label: label,
+    kind: CompletionItemKind.Method,
+    detail: `Detail for ${label} is still TODO :(`,
+    documentation: `Documentation for ${label} is still TODO :(`,
+  };
+}
+
+/// List of completions that can be inserted in the document.
+const completions: CompletionItem[] = [
+  createImportCompletion("#.IMPORT"),
+  createImportCompletion("#.INCLUDE"),
+  createRenameCompletion("#.TYPE_RENAME", "types"),
+  createRenameCompletion("#.FIELD_RENAME", "fields"),
+  createRenameCompletion("#.TF_RENAME", "types and fields"),
+  createTodoCompletionItem("#.END_OF_CNF"),
+  createTodoCompletionItem("#.OPT"),
+  createTodoCompletionItem("#.PDU"),
+  createTodoCompletionItem("#.REGISTER"),
+  createTodoCompletionItem("#.MODULE"),
+  createTodoCompletionItem("#.MODULE_IMPORT"),
+  createTodoCompletionItem("#.OMIT_ASSIGNMENT"),
+  createTodoCompletionItem("#.NO_OMIT_ASSGN"), // can't find anything about it
+  createTodoCompletionItem("#.VIRTUAL_ASSGN"),
+  createTodoCompletionItem("#.SET_TYPE"),
+  createTodoCompletionItem("#.ASSIGN_VALUE_TO_TYPE"),
+  createTodoCompletionItem("#.IMPORT_TAG"),
+  createTodoCompletionItem("#.TYPE_ATTR"),
+  createTodoCompletionItem("#.ETYPE_ATTR"), // can't find anything about it
+  createTodoCompletionItem("#.FIELD_ATTR"),
+  createTodoCompletionItem("#.EFIELD_ATTR"), // can't find anything about it
+  createTodoCompletionItem("#.SYNTAX"),
+  createTodoCompletionItem("#.OMIT_ALL_ASSIGNMENTS"), // can't find anything about it
+  createTodoCompletionItem("#.OMIT_ASSIGNMENTS_EXCEPT"), // check how it works
+  createTodoCompletionItem("#.OMIT_ALL_TYPE_ASSIGNMENTS"), // can't find anything about it
+  createTodoCompletionItem("#.OMIT_TYPE_ASSIGNMENTS_EXCEPT"), // can't find anything about it
+  createTodoCompletionItem("#.OMIT_ALL_VALUE_ASSIGNMENTS"), // can't find anything about it
+  createTodoCompletionItem("#.OMIT_VALUE_ASSIGNMENTS_EXCEPT"), // can't find anything about it
+  createTodoCompletionItem("#.EXPORTS"),
+  createTodoCompletionItem("#.MODULE_EXPORTS"),
+  createTodoCompletionItem("#.USER_DEFINED"), // can't find anything about it
+  createTodoCompletionItem("#.NO_EMIT"),
+  createTodoCompletionItem("#.MAKE_ENUM"),
+  createTodoCompletionItem("#.MAKE_DEFINES"),
+  createTodoCompletionItem("#.USE_VALS_EXT"),
+  createTodoCompletionItem("#.FN_HDR"),
+  createTodoCompletionItem("#.FN_FTR"),
+  createTodoCompletionItem("#.FN_BODY"),
+  createTodoCompletionItem("#.FN_PARS"),
+  createTodoCompletionItem("#.CLASS"),
+  createTodoCompletionItem("#.ASSIGNED_OBJECT_IDENTIFIER"),
+  createTodoCompletionItem("#.END"),
+];
+
+/// Returns a list of completion items for the given document and position. The list is based on the
+/// text before the cursor and the list of completions. The text before the cursor is used to determine
+/// the completion prefix and the range of text that should be replaced by the completion.
+/// @param document The document for which completions are requested.
+/// @param position The position in the document where completions are requested.
+/// @returns A list of completion items.
 function getCompletionItems(document: TextDocument, position: Position): CompletionItem[] {
   const line = document.getText({
     start: { line: position.line, character: 0 },
@@ -48,6 +115,10 @@ function getCompletionItems(document: TextDocument, position: Position): Complet
   }));
 }
 
+/// Handles a completion request.
+/// @param documents The collection of documents.
+/// @param textDocumentPosition The position in the document where completions are requested.
+/// @returns A list of completion items.
 export function onCompletion(
   documents: TextDocuments<TextDocument>,
   textDocumentPosition: TextDocumentPositionParams,
@@ -67,6 +138,10 @@ export function onCompletion(
   return completionItems;
 }
 
+/// Handles a completion resolve request. This function is called when a completion item is selected
+/// by the user and the client requests additional information about the completion item.
+/// @param item The completion item that should be resolved.
+/// @returns The resolved completion item.
 export function onCompletionResolve(item: CompletionItem) {
   log.write("Completion resolve request received");
 

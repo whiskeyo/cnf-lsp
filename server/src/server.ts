96b1d59bc6845bd68707222734b82fb7b06d6fcd
Tomasz Woszczynski <duchowe50k@gmail.com>
2025-03-17 22:37:23 +0100

    Add debounce from lodash to delay onDidChangeContent trigger

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,8 @@ import {
   TextDocumentPositionParams,
 } from "vscode-languageserver/node";
 
+import { debounce } from "lodash";
+
 import { onCompletion, onCompletionResolve } from "./handlers/completion";
 import { onDocumentChange } from "./handlers/diagnostic";
 import { TextDocument } from "vscode-languageserver-textdocument";
@@ -14,12 +16,17 @@ import { onInitialize } from "./handlers/initialize";
 
 const connection = createConnection(ProposedFeatures.all);
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+const debounceTimeout = 100; // milliseconds
 
 connection.onInitialize((params: InitializeParams) => onInitialize(params));
 connection.onCompletion((params: TextDocumentPositionParams) => onCompletion(documents, params));
 connection.onCompletionResolve((item: CompletionItem) => onCompletionResolve(item));
 
-documents.onDidChangeContent((change) => onDocumentChange(connection, change));
+const onDidChangeContentDebounced = debounce(
+  (connection, change) => onDocumentChange(connection, change),
+  debounceTimeout,
+);
+documents.onDidChangeContent((change) => onDidChangeContentDebounced(connection, change));
 
 documents.listen(connection);
 connection.listen();
